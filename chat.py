550764b6333976409917cd7525cda28418a708d5
KhresnaPanduI <khresna.pandu10@gmail.com>
2024-02-02 23:09:36 +0700

    remove debugging print

--- chat.py
+++ chat.py
@@ -4,25 +4,22 @@ import gradio as gr
 MODEL_PATH = r"C:\Users\Aevum\Documents\Belajar\Coding\LLM\mistral-7b-instruct-v0.2.Q4_K_M.gguf"
 
 llm = Llama(
-    model_path=MODEL_PATH
-    )
+    model_path=r"C:\Users\Aevum\Documents\Belajar\Coding\LLM\mistral-7b-instruct-v0.2.Q4_K_M.gguf", 
+    verbose=True,
+    chat_format="llama-2")  # Set chat_format according to the model you are using
 
 # Initialize the global variable for Llama history
-llama_history_global = []
+llama_history_global = [{"role": "system", "content": "You are helpful assistant."}]
+#llama_history_global = []
 
 def gradio_reply(user_input, history):
     global llama_history_global
     
     # Append the user's input to Llama history
     llama_history_global.append({"role": "user", "content": f"{user_input} answer concisely"})
-
-    stream = llm.create_chat_completion(messages=llama_history_global, stream=True)
-
-    assistant_response = ""
-    for item in stream:
-        if 'content' in item['choices'][0]['delta']:
-            content = item['choices'][0]['delta']['content']
-            assistant_response += content
+    # Add assitant response
+    response = llm.create_chat_completion(messages=llama_history_global)
+    assistant_response = response['choices'][0]['message']['content']
 
     # Append the assistant's response to history for Gradio
     history.append((user_input, assistant_response))
