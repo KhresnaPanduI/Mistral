6ba81035f4ac982f33851d07866512409e548ef7
KhresnaPanduI <khresna.pandu10@gmail.com>
2024-02-29 22:28:40 +0700

    add text splitter

--- pdf.py
+++ pdf.py
@@ -1,5 +1,6 @@
 from PyPDF2 import PdfReader
-
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
 
 def parse_pdf_to_text(pdf_file):
     if pdf_file is not None:
@@ -8,4 +9,11 @@ def parse_pdf_to_text(pdf_file):
         text = ""
         for page in pdf_reader.pages:
             text += page.extract_text()
+        
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1000,
+            chunk_overlap=200,
+            length_function=len
+        )
+        chunks = text_splitter.split_text(text=text)
         
